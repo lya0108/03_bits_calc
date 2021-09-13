efd10d536bb625c4fe5467a67781d11f429b35f9
lya0108 <lya0108@masseyhigh.school.nz>
2021-09-14 09:59:07 +1200

    Create 01_statement_generator.py

---
+++ 01_statement_generator.py
@@ -0,0 +1,13 @@
+def statement_generator(text, decoration):
+
+    ends = decoration * 5
+
+    statement = "{} {} {}".format(ends, text, ends)
+
+    print()
+    print(statement)
+    print()
+
+    return ""
+
+statement_generator("hello world", "-")
