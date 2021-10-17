845355ca28e81c104e9a868e0799be050962876a
lya0108 <lya0108@masseyhigh.school.nz>
2021-10-18 12:45:12 +1300

    Update 03_integer_checker.py

--- 03_integer_checker.py
+++ 03_integer_checker.py
@@ -24,6 +24,9 @@ keep_going = ""
 while keep_going == "":
     print()
     # asks user for integer, width, and height
-    var_integer = num_check()
-    width = num_check()
-    height = num_check()
+    var_integer = num_check("enter an integer: ", 0)
+    print()
+    # asks user for width and height of an image
+    width = num_check("image width? ", 1)
+    print()
+    height = num_check("image height?", 1)
